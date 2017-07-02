289a9429d2f9bf4407b8821d48fcf50bd708d2fc
Patrick <contato@widedevelop.com>
2017-07-01 21:59:03 -0300

    Ajustes no slug

--- slug/Slug.php
+++ slug/Slug.php
@@ -1,38 +1,52 @@
 <?php
 
-class Slug {
+class Slug
+{
 
-    public function input($value, $field, $fields) {
+    public function input($value, $field, $data)
+    {
+        $section = $data['section'];
+        $fields = $data['fields'];
         $CI = &get_instance();
-        $field_trigger = search($fields, 'plugins', 'slug_trigger\|', true);
-        if (isset($field_trigger[0])) {
-            $field_trigger = $field_trigger[0];
-            $value_trigger = $CI->input->post($field_trigger['column']);
-            $slug = $this->set_slug($value_trigger, $field_trigger);
-            return $slug;
-        } else {
-            return slug($value);
+        $field_trigger = $this->search_trigger($fields);
+        $slug = slug($value);
+        if (isset($field_trigger)) {
+            $value_trigger = $CI->input->post($field_trigger['database']['column']);
+            $slug = $this->set_slug($value_trigger, $field_trigger, $section);
         }
-    }
 
-    private function set_slug($value, $field) {
-        $section = get_section();
-        $slug_ = slug($value);
-        $CI = &get_instance();
-        $id_post = $CI->uri->segment(8);
-        $CI->load->model_app('crud_model');
-        $x = 0;
-        $exists = true;
-        $column = $field['column'];
-        while ($exists == true) {
-            $slug = $slug_;
-            if ($x > 0) {
-                $slug = $slug . $x;
-            }
-            $exists = $CI->crud_model->read($section['table'], null, array($column => $slug, 'id!=' => $id_post))->row();
-            $x++;
-        }
         return $slug;
     }
 
+    private function search_trigger($fields)
+    {
+        foreach ($fields as $field) {
+            if (isset($field['input']['plugins']) && $field['input']['plugins'] == 'slug_trigger') {
+                return $field;
+            }
+        }
+    }
+
+    private function set_slug($value, $field, $section)
+    {
+        $slug = slug($value);
+        $CI = &get_instance();
+        $CI->load->database();
+        $id_post = $CI->uri->segment(8);
+        $x = 0;
+        $exists = true;
+
+        $column = $field['database']['column'];
+        while ($exists == true) {
+            $current_slug = $slug;
+            if ($x > 0) {
+                $current_slug = $current_slug . $x;
+            }
+
+            $exists = $CI->db->get_where($section['table'], array('id!=' => $id_post, $column => $current_slug))->row();
+            $x++;
+        }
+
+        return $current_slug;
+    }
 }
